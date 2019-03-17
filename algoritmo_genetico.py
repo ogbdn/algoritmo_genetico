d565d241722a7e5ccc122c60d1409b3d3b7674c9
Othon Guilherme Berardo Dubeux Nin <Othon@MacBook-Pro-de-Othon.local>
2019-03-17 13:51:42 -0300

    Construir nova geração

--- algoritmo_genetico.py
+++ algoritmo_genetico.py
@@ -47,14 +47,14 @@ class Individuo():
         return filhos
     
     def mutacao(self, taxa_mutacao):
-        print("Antes %s " % self.cromossomo)
+        #print("Antes %s " % self.cromossomo)
         for i in range(len(self.cromossomo)):
             if random() < taxa_mutacao:
                 if self.cromossomo[i] == '1':
                     self.cromossomo[i] = '0'
                 else:
                     self.cromossomo[i] = '1'
-        print("Depois %s " % self.cromossomo)
+        #print("Depois %s " % self.cromossomo)
         return self
         
 class AlgoritmoGenetico():
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     lista_produtos.append(Produto("TV 55' ", 0.400, 4346.99))
     lista_produtos.append(Produto("TV 50' ", 0.290, 3999.90))
     lista_produtos.append(Produto("TV 42' ", 0.200, 2999.00))
-    lista_produtos.append(Produto("Notebook Dell", 0.00350, 2499.90))
+    '''lista_produtos.append(Produto("Notebook Dell", 0.00350, 2499.90))
     lista_produtos.append(Produto("Ventilador Panasonic", 0.496, 199.90))
     lista_produtos.append(Produto("Microondas Electrolux", 0.0424, 308.66))
     lista_produtos.append(Produto("Microondas LG", 0.0544, 429.90))
@@ -111,7 +111,7 @@ if __name__ == '__main__':
     lista_produtos.append(Produto("Geladeira Brastemp", 0.635, 849.00))
     lista_produtos.append(Produto("Geladeira Consul", 0.870, 1199.89))
     lista_produtos.append(Produto("Notebook Lenovo", 0.498, 1999.90))
-    lista_produtos.append(Produto("Notebook Asus", 0.527, 3999.00))
+    lista_produtos.append(Produto("Notebook Asus", 0.527, 3999.00))'''
     
     espacos = []
     valores = []
@@ -122,7 +122,7 @@ if __name__ == '__main__':
         nomes.append(produto.nome)
     limite = 3
     
-    tamanho_populacao = 20
+    tamanho_populacao = 4
     ag = AlgoritmoGenetico(tamanho_populacao)
     ag.inicializa_populacao(espacos, valores, limite)
     for individuo in ag.populacao:
@@ -130,9 +130,24 @@ if __name__ == '__main__':
     ag.ordena_populacao()
     ag.melhor_individuo(ag.populacao[0])
     soma = ag.soma_avaliacoes()
+    nova_populacao = []
+    probabilidade_mutacao = 0.01
     for individuos_gerados in range(0, ag.tamanho_populacao, 2):
         pai1 = ag.seleciona_pai(soma)
         pai2 = ag.seleciona_pai(soma)
+        
+        filhos = ag.populacao[pai1].crossover(ag.populacao[pai2])
+        nova_populacao.append(filhos[0].mutacao(probabilidade_mutacao))
+        nova_populacao.append(filhos[1].mutacao(probabilidade_mutacao))
+        
+    ag.populacao = list(nova_populacao)
+    for individuo in ag.populacao:
+        individuo.avaliacao()
+    ag.ordena_populacao()
+    ag.melhor_individuo(ag.populacao[0])        
+    soma = ag.soma_avaliacoes()
+    
+    print("Melhor: %s" % ag.melhor_solucao.cromossomo, "Valor: %s\n" % ag.melhor_solucao.nota_avaliacao)
         
         
         
